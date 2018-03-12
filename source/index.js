3433e92281d1e12076b7dc5a471310ebcb31413b
stormsen <micha@edit23.de>
2018-03-12 23:48:51 +0100

    Make Object Matrix!

--- source/index.js
+++ source/index.js
@@ -21,7 +21,7 @@ import SceneSetupBase from './demos/sceneSetupBase';
 
 const init = () => {
 
-    const demo = new SceneSetupBase();
+    const demo = new ColladaImport();
 
     const update = () => {
         raf(update);
